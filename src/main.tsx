eaefeb6f592ce5093a5059e2bb90ec31e5c95422
Svetlana Andreeva <AnbreevaSveta@yandex.ru>
2026-09-06 18:43:03 +0500

    Enable hero slideshow and top admin link

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { createRoot } from "react-dom/client";
 import App, { AdminApp } from "./app/App.tsx";
 import "./styles/index.css";
 import "./styles/featured-switcher.css";
+import "./styles/storefront-enhancements.css";
 import { startFeaturedSwitcher } from "./featuredSwitcher";
+import { startStorefrontEnhancements } from "./storefrontEnhancements";
 
 const isAdmin = window.location.pathname.startsWith("/admin");
 createRoot(document.getElementById("root")!).render(isAdmin ? <AdminApp /> : <App />);
 
-if (!isAdmin) startFeaturedSwitcher();
+if (!isAdmin) {
+  startFeaturedSwitcher();
+  startStorefrontEnhancements();
+}
