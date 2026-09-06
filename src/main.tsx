f0c6969d2781dfcb439e9a833258fb70a119d081
Svetlana Andreeva <AnbreevaSveta@yandex.ru>
2026-09-06 19:20:28 +0500

    Apply mobile hero width override

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App, { AdminApp } from "./app/App.tsx";
 import "./styles/index.css";
 import "./styles/featured-switcher.css";
 import "./styles/storefront-enhancements.css";
+import "./styles/mobile-hero-width-fix.css";
 import { startFeaturedSwitcher } from "./featuredSwitcher";
 import { startStorefrontEnhancements } from "./storefrontEnhancements";
 import { startBrandCopyEnhancements } from "./brandCopyEnhancements";
